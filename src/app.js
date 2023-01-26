1e34912a0099fe7cd70898a607d3c2f241abd86c
Tóth Fedor <tothfedorlaszlo@ktch.hu>
2023-01-26 20:11:51 +0100

    VÉgső

--- src/app.js
+++ src/app.js
@@ -7,3 +7,30 @@
 * Github: https://github.com/TothFL/
 * Licenc: GNU GPL
 */
+
+const asidaElem = document.querySelector("#aside");
+const bsidaElem = document.querySelector("#bside");
+const csidaElem = document.querySelector("#cside");
+const dsidaElem = document.querySelector("#dside");
+const calcButton = document.querySelector("#calcbutton");
+const areaElem= document.querySelector("#area");
+
+calcButton.addEventListener('click', ()=>{
+    //console.log('működik')
+    let aside = Number(asidaElem.value);
+    let bside = Number(bsidaElem.value);
+    let cside = Number(csidaElem.value);
+    let dside = Number(dsidaElem.value);
+    let area = calcArea(aside, bside, cside, dside);
+    //console.log(area);
+    //areaElem.value=areakerek
+    let areakerek = parseFloat(area).toFixed(2);
+    areaElem.value=areakerek
+
+});
+
+function calcArea(a, b, c, d) {
+    let terulet = ((a+c)/(4*(a-c))) * Math.sqrt((a+b-c+d)*(a-b-c+d)*(a+b-c-d)*(-a+b+c+d));
+    return terulet;
+}
+
