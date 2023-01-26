6af9035d430b0d8389b6401bc43c946e8def858f
Tóth Fedor <tothfedorlaszlo@ktch.hu>
2023-01-26 19:31:43 +0100

    Webes UI kész

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+* File: app.js
+* Author: Tóth Fedor László
+* Copyright: 2023, Tóth Fedor László
+* Group: Szoft I-2-E
+* Date: 2023.01.26
+* Github: https://github.com/TothFL/
+* Licenc: GNU GPL
+*/
